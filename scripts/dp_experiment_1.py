d729cf34a72c0849e0de16eac3c7052c39c627d2
codett <casey.dettlaff@gmail.com>
2026-03-23 14:39:19 -0700

    Refactored plotting code, got rid of unused functions, added functionality to save plots.

--- scripts/dp_experiment_1.py
+++ scripts/dp_experiment_1.py
@@ -2,7 +2,7 @@ import pandas as pd
 from data.loadData import load_data, process_data, split_data, window_data, unwindow_data
 from model.testMdlPM import testMdlPM, evaluate_prediction, energy_accuracy, get_results
 from model.trainMdlPM import trainMdlPM
-from general.analysis_and_visualization import plot_accuracy_versus_epsilon, plot_theoretical_eacc_bound, plot_results_with_theoretical_bound
+from general.analysis_and_visualization import plot_results_with_theoretical_bound
 from differential_privacy import differential_privacy
 import os
 import numpy as np

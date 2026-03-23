636a667ab16205ab09e35cefc85e9a9902094515
codett <casey.dettlaff@gmail.com>
2026-03-23 14:56:02 -0700

    moved features function into pattern matching script

--- src/pattern_matching.py
+++ src/pattern_matching.py
@@ -4,6 +4,66 @@
 from numpy import savez_compressed
 import numpy as np
 from tqdm import tqdm
+from scipy import stats
+
+def features(data, feature_selection):
+
+    if 'all' in feature_selection: F = 14
+    else: F = len(feature_selection)
+    N,T = data.shape
+    out = np.zeros((N, F))
+    idx = 0
+
+    if 'Mean' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.mean(data, axis=1)
+        idx = idx + 1
+    if 'Std' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.std(data, axis=1)
+        idx = idx + 1
+    if 'RMS' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.sqrt(np.mean(data ** 2, axis=1))
+        idx = idx + 1
+    if 'Peak2Rms' in feature_selection or 'all' in feature_selection:
+        temp = np.max(data, axis=1)
+        temp2 = np.sqrt(np.mean(data ** 2, axis=1))
+        out[:, idx] = np.divide(temp, temp2)
+        idx = idx + 1
+    if 'Median' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.median(data, axis=1)
+        idx = idx + 1
+    if 'Min' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.min(data, axis=1)
+        idx = idx + 1
+    if 'Max' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.max(data, axis=1)
+        idx = idx + 1
+    if 'Per25' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.percentile(data, 25, axis=1)
+        idx = idx + 1
+    if 'Per75' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.percentile(data, 75, axis=1)
+        idx = idx + 1
+    if 'Energy' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.mean(data, axis=1)
+        idx = idx + 1
+    if 'Var' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.var(data, axis=1)
+        idx = idx + 1
+    if 'Range' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = np.ptp(data, axis=1)
+        idx = idx + 1
+    if '3rdMoment' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = stats.skew(data, axis=1)
+        idx = idx + 1
+    if '4thMoment' in feature_selection or 'all' in feature_selection:
+        out[:, idx] = stats.kurtosis(data, axis=1)
+        idx = idx + 1
+
+    # Post-processing
+    out = np.nan_to_num(out)
+    out[out == np.inf] = 0
+
+    return out
 
 def trainMdlPM(data_train, mdl_filepath, save_mdl=False, return_mdl=False):
 
@@ -118,8 +178,8 @@ def testMdlPM(X_test, Y_test, mdl, feature_selection=None, C=0.01):
 
     # Feature Extraction
     if feature_selection:
-        features_mdl = features2D(mdl[:, :, 0], feature_selection)
-        features_X = features2D(X_test, feature_selection)
+        features_mdl = features(mdl[:, :, 0], feature_selection)
+        features_X = features(X_test, feature_selection)
 
     sel_list = [] # For debugging
 

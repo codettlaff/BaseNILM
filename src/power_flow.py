cce4f2b310d7de8181f3b16d413c805f7132cc01
codett <casey.dettlaff@gmail.com>
2026-04-08 15:39:05 -0700

    Finished revising init function

--- src/power_flow.py
+++ src/power_flow.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 class RadialNetwork:
-    def __init__(self, nodes, edges, root=0, V0=1.0, alpha=1.0):
+    def __init__(self, nodes, edges, root=0, V0=1.0, alpha=1.0, epsilon=None):
         """
-        nodes : list of node indices
+        nodes : dict {i: {"P": P_i, "B": B_i}}
         edges : list of (i, j, r_ij, x_ij)
         root  : root node
         V0    : root node voltage
         alpha : constant power factor parameter (Q_i = alpha P_i)
         """
-        self.nodes = nodes
+        self.nodes = list(nodes.keys())  # Node indices
+        self.P = {i: data["P"] for i, data in nodes.items()} # Nodal active power injections
+        self.B = {i: data["B"] for i, data in nodes.items()} # Appliance power bound
         self.root = root
         self.V0 = V0
         self.alpha = alpha
 
         # Tree structure
-        self.children = {i: [] for i in nodes}
+        self.children = {i: [] for i in self.nodes}
         self.parent = {}
+        self.lines = []
 
         # Line parameters
         self.r = {}
@@ -25,8 +28,27 @@ class RadialNetwork:
         for i, j, r_ij, x_ij in edges:
             self.children[i].append(j)
             self.parent[j] = i
+            self.lines.append((i, j))
             self.r[(i, j)] = r_ij
             self.x[(i, j)] = x_ij
+
+        self.p = [] # list of floats active power branch flows. empty until power flow solved
+        self.V = [] # list of floats nodal voltage injections. empty until power flow solved
+        self.v = [] # list of floats branch voltage drops, empty until power flow solved
+
+        # Privacy Stuff
+        if epsilon: self.do_differential_privacy = True
+        else: self.do_differential_privacy = False
+        self.epsilon = epsilon
+        self.eta = [] # list of noise added to active power injection at each node, empty until differntial privacy calculated
+
+        self.p_tilde = [] # list of noisy active power branch flows. empty until power flow solved
+        self.V_tilde = [] # list of noisy floats nodal voltage injections. empty until power flow solved
+        self.v_tilde = [] # list of noisy branch voltage drops, empty until power flow solved
+
+        self.e_i = [] # list of line current flow error, empty until error calculated
+        self.e_p = [] # list of line power flow error, empty until error calculated
+        self.e_V = [] # list of nodal voltage error, empty until error calculated
 
     # ------------------------------------------------------------------
     # C(i):Set of all nodes along path from root to node.

b168b29a4aa4a85ff9d63bc7f175f4ec2eab124a
codett <casey.dettlaff@gmail.com>
2026-04-29 14:51:09 -0700

    Wrote code to construct ieee123bus nodes from redd houses

--- scripts/123bus_power_flow_experiment.py
+++ scripts/123bus_power_flow_experiment.py
@@ -1,6 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 from data.loadData import load_data, process_data
 from power_flow_with_dss_refactored import RadialNetwork
@@ -75,14 +76,49 @@ def load_redd_houses():
 
 def max_power_per_house(houses): return [np.max(house["P"]) for house in houses]
 
+def assign_houses_to_loads(houses, target_loads, tol=0.10, max_iter=1000):
+    house_powers_kw = [np.max(h["P"]) / 1000 for h in houses]
+
+    assignments = []
+    achieved_kw = []
+    house_counts = []  # new list
+
+    for target in target_loads:
+        lower = target * (1 - tol)
+        upper = target * (1 + tol)
+
+        total = 0.0
+        chosen = []
+        iters = 0
+
+        while total < lower and iters < max_iter:
+            i = random.randrange(len(houses))
+            total += house_powers_kw[i]
+            chosen.append(houses[i])
+            iters += 1
+
+            # If overshoot too much, restart
+            if total > upper:
+                total = 0.0
+                chosen = []
+                iters = 0
+
+        assignments.append(chosen)
+        achieved_kw.append(total)
+        house_counts.append(len(chosen))  # track number of houses
+
+    return assignments, achieved_kw, house_counts
+
 houses = load_redd_houses()
 max_powers = max_power_per_house(houses)
+load_powers = [13.33, 6.67, 35, 70, 46.67, 25, 81.67]
+assignments, achieved_kw, house_counts = assign_houses_to_loads(houses, load_powers)
 # max powers = [348.08, 38.35, 781.45, 1845.93, 552,18, 313.24]
 
 print('')
 
 # Build Loads
-# DSS 123Bus Original Loads
+# DSS 123Bus Original Loads - 8 Unique Loads
 # Load 1 - 13.33 kw, 6.667 kVAr
 # Load 2 - 6.667 kW, 3.333 kVAr
 # Load 3 - 35 kw, 25 kVAr
@@ -93,3 +129,6 @@ print('')
 # Load 8 - 81.67 kw, 60 kVAr
 
 # 6 REDD Houses (W): [348.08, 38.35, 781.45, 1845.93, 552,18, 313.24]
+# Combine the REDD houses to get loads with same magnitudes as original 123Bus system loads.
+
+# For each Load, add random house from set of 6 houses until load is within +/- 10% of desired list
